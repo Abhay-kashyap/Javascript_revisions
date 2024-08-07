4a6eaf49dae4ad9d324671013a56da0fcabdea5e
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-08-07 14:07:56 +0530

    add class and object

--- classAndObject/script.js
+++ classAndObject/script.js
@@ -10,14 +10,18 @@ class Product{
         this.name=n;
         this.price=p;
         this.rating=r;
+        // return 10  ; if your are returning primitive value then it will be avoided in constructor
+        
+        // return {x:0,y:20} // if you are return non primitive then it will be returned
     }
+   
     // behaviour -> function -> member function
 display(){
     // console.log('this object',this)
-    console.log('display the product')
+    console.log('display the current product',this.name,this.price,this.rating)
 }
 }
 const p =new Product('iphone' ,150000,5);//  new -> create an  empty plain object
 // -> in the above piece of code we are calling the constructor  method
 console.log(p);
-p.display()
+// p.display()
