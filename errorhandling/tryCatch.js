cf97a7dbcb8ded26b1dac74713ac2b5dfa71e821
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-08-12 13:29:37 +0530

    tryCatch file update

--- errorhandling/tryCatch.js
+++ errorhandling/tryCatch.js
@@ -1,10 +1,37 @@
-try{
-    console.log('hello');
-    console.log(a);
-    console.log('end try')
-}catch(e){
-    console.log('error handled')
-}finally{
-    console.log('close my file')
+// try{
+//     console.log('hello');
+//     console.log(a);
+//     console.log('end try')
+// }catch(e){
+//     console.log('error handled')
+// }finally{
+//     console.log('close my file')
+// }
+// console.log('end')
+
+
+function getPerson(obj){
+    try{
+        if(typeof obj ==='object' && obj!==null && 'name' in obj && 'age' in obj){
+            return `Name ${obj.name} and Age ${obj.age}`
+        }
+        throw new Error('Invalid Parameter Type')
+    }catch(e){
+        return e.message
+    }
 }
-console.log('end')
+console.log(getPerson({name:'Mithun',age:20}))
+console.log(getPerson({name:'Mithun'}))
+console.log(getPerson(['name','mithun']))
+
+// class car{
+//     company='Skoda';
+//     model='rapid'
+//     year=2022;
+//     getDescription() {
+//         return `This is a ${this.year} ${this.company} ${this.model}`
+//     }
+
+//     }
+// const myCar=new car();
+// console.log(myCar.getDescription())
