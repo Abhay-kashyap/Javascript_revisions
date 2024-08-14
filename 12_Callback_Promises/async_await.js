db415ff3327a121eabfb812f2fdad2bce183cbc1
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-08-14 14:29:30 +0530

    add localstorage file

--- 12_Callback_Promises/async_await.js
+++ 12_Callback_Promises/async_await.js
@@ -19,3 +19,12 @@ console.log('start');
 
 console.log(consume());
 console.log('end');
+
+fetch('https://type.fit/api/quotes').then(function(response){
+    return response.json() 
+}).then(function(data){
+    console.log(data)
+    data.forEach((c)=>{
+        console.log(c)
+    })
+})
