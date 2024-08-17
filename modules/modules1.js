6965e6e09676637453014175e48dca36ff98e532
Abhay-kashyap <abhaykashyap265@gmail.com>
2024-08-17 23:56:44 +0530

    add modules with common js

---
+++ modules/modules1.js
@@ -0,0 +1,13 @@
+// in a javascript we have two types modules
+ const hello=()=>{
+    console.log('hello today we learn modules');
+}
+ const Ahello=(name)=>{
+    console.log('hello today we learn modules with', name);
+}
+module.exports={hello,Ahello};//same as below line
+// module.exports={hello:hello,Ahello:Ahello}
+
+
+
+// ES 6 modules
---
+++ modules/modules2.js
@@ -0,0 +1,8 @@
+// const {hello ,Ahello}=require('./modules1');
+// hello()
+// Ahello('Harry')
+// Ahello('Shivam');
+// Ahello('Vishal')
+// Ahello('Ayush')
+
+import { hello } from "./modules3";
